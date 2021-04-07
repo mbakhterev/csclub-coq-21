b6b8dde912b26022cd11ab20fa985cf34dbc5967
mbakhterev <mob@k.imm.uran.ru>
2021-04-07 20:12:23 +0500

    WIP: ДЗ 4

--- hw04.v
+++ hw04.v
@@ -6,8 +6,8 @@ Unset Printing Implicit Defensive.
 
 (* An (unsound) placeholder so that the whole file typechecks.
 Please replace it with your proof term. Your solutions may not
-use any axioms, including `replace_with_your_solution_here` *)
-Axiom replace_with_your_solution_here : forall {A : Type}, A.
+use any axioms, including `replace_with_your_solution_here` 
+Axiom replace_with_your_solution_here : forall {A : Type}, A. *)
 
 
 (* WARNING!
@@ -35,7 +35,8 @@ Axiom replace_with_your_solution_here : forall {A : Type}, A.
 
 Definition and_via_ex (A B : Prop) :
   (exists (_ : A), B) <-> A /\ B
-:= replace_with_your_solution_here.
+:= conj (fun '(ex_intro a b) => conj a b)
+        (fun '(conj a b) => ex_intro (fun _ => B) a b).
 
 
 (** * Exercise *)
@@ -54,16 +55,25 @@ Definition refl_if :
   (forall x y, R x y -> R y x) ->
   (forall x y z, R x y -> R y z -> R x z) ->
   forall x : D, (exists y, R x y) -> R x x
-:= replace_with_your_solution_here.
+:= fun r_symm r_trans x '(ex_intro y r_xy) =>
+   let r_yx := r_symm x y r_xy in r_trans x y x r_xy r_yx.
 
 End Symmetric_Transitive_Relation.
 
 
 (** * Exercise *)
+Reset pair_inj.
 Definition pair_inj A B (a1 a2 : A) (b1 b2 : B) :
   (a1, b1) = (a2, b2) -> (a1 = a2) /\ (b1 = b2)
-:= replace_with_your_solution_here.
+:= fun eq_pairs =>
+   match eq_pairs in (_ = ab2) return ((a1 = fst ab2) /\ (b1 = snd ab2)) with
+   | erefl => conj erefl erefl
+   end.
 
+
+Print pair.
+Compute (fst (10, 20)).
+Unset Printing Notations.
 
 (** * Exercise *)
 Inductive trilean : Type :=
@@ -71,22 +81,93 @@ Inductive trilean : Type :=
   | No
   | Maybe.
 
+Reset yes_no_disj.
 Definition yes_no_disj :
   Yes <> No
-:= replace_with_your_solution_here.
+:= fun eqen =>
+   match eqen in (_ = n) return (if n is Yes then True else False) with
+   | erefl => I
+   end. 
 
 
 (** * Exercise *)
-Definition addA : associative addn
-:= replace_with_your_solution_here.
 
+Print associative.
+Print nat_ind.
+
+Fixpoint dummy (n : nat) : nat := match n with 0 => 0 | S k => dummy k end.
+Print dummy.
+Check addnS.
+Check eq_trans.
+Check eq_sym.
+Check congr1.
+Locate eq_trans.
+Locate addnS.
+
+Unset Printing Notations.
+
+(* (S m) + n = S (n + m) *)
+
+Definition addnSl : forall (m n : nat), eq (addn (S m) n) (S (addn m n)) :=
+fix ih m n :=
+match m with
+| O => erefl
+| S m' => let ih' := ih m' n in
+          let sm_n_ss_mn := congr1 S ih' in
+          let ss_mn_ssmn : eq (addn (S (S m')) n) (S (S (addn m' n))) := erefl
+          in eq_trans_r ss_mn_ssmn sm_n_ss_mn
+end.
+
+(* m + 0 = m *)
+
+Definition addnOl : forall n : nat, eq (addn n O) n :=
+fix ih n :=
+match n with
+| O => erefl
+| S n' => let sno_s_no : eq (addn (S n') O) (S (addn n' O)) := erefl in
+          let ih' := ih n' in
+          let sno_sn := congr1 S ih'
+          in eq_trans sno_s_no sno_sn
+end.
+
+Definition addA : associative addn
+:= fix ih x y z :=
+   match z with
+   | 0 => match x with
+          | 0 => erefl
+          | S x' => let sx_yo_sxy := congr1 (addn (S x')) (addnOl y) in
+                    let sxy_o_sxy := addnOl (addn (S x') y) in
+                    eq_trans_r sx_yo_sxy sxy_o_sxy
+          end
+   | S z' => let ih' := ih x y z' in
+             let Sxyz := addnS (addn x y) z' in
+             let iSxyz := congr1 S ih' in
+             let xSyz := addnS x (addn y z') in
+             let xySz := congr1 (addn x) (addnS y z') in
+             let xySz_Syxz := eq_trans xySz xSyz in
+             let xy_Sz_Syxz := eq_trans_r Sxyz iSxyz in
+             eq_trans_r xySz_Syxz xy_Sz_Syxz
+   end.
 
 (** * Exercise: *)
-Definition addnCA : left_commutative addn
-:= replace_with_your_solution_here.
+
+Check left_commutative addn.
+Print left_commutative.
+Print eq_trans_r.
+
+Definition addnCA : left_commutative addn :=
+fix ih x y z :=
+match x with
+| O => erefl
+| S x' => let ih' := ih x' y z in
+          let sx_yz_sxyz := addnSl x' (addn y z) in
+          let y_sxz_syxz := eq_trans (congr1 (addn y) (addnSl x' z)) (addnS y (addn x' z)) in 
+          let iscongr := congr1 S ih' in
+          let H := eq_trans sx_yz_sxyz iscongr in
+          eq_trans_r H y_sxz_syxz
+end.
 
 (* Hint: re-use addnS lemma and some general lemmas about equality *)
-
 
 
 
